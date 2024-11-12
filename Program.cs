097844e83495d54cf01431efd7f8e0dc6707d746
Kab-35-16 <Kab-35-16@KAB-32-1->
2024-11-12 14:31:50 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Привет");
+        }
+        public static void SayBuy()
+        {
+            Console.WriteLine("Пока");
+        }
+        static void Main(string[] args)
+        {
+            //Задача 1.Вывод  сообщений приветствия
+            //и прощания с использованием дополнительных методов.
+            SayHello();
+            SayBuy();
+            Console.ReadKey();
+        }
+    }
+}
